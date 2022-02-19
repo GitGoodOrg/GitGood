87db532b8a1a111711eea8536e460d97e370a382
Mark Charles Smith <markcharlessmith@gmail.com>
2022-02-19 18:39:16 -0500

    added form submission feature (topicSubmit and cardSubmit)

--- client/containers/Nav.jsx
+++ client/containers/Nav.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
-import Topic from "../components/Topic.jsx"
+import React, { useState } from 'react';
+import Topic from '../components/Topic.jsx'
 
 function Nav(props) {
-const topicsFeed = [];
-// iterate through props.topics
-for (let i = 0; i < props.topics.length; i++) {
+  const topicsFeed = [];
+  // iterate through props.topics
+  for (let i = 0; i < props.topics.length; i++) {
     topicsFeed.push(<Topic key={i} topics={props.topics[i]} />);
-}
+  }
 
   return(
     <div>
       <h2>Nav</h2>
-      <form action="" onSubmit={props.topicSubmit}>
-        <input type="submit" />
+      <form action='' onSubmit={(e) => props.topicSubmit(e)}>
+        <input type='text' onChange={(e) => props.topicTextEntry(e)} value={props.topicText}/>
+        <input type='submit' />
       </form> 
       {topicsFeed}
     </div>
