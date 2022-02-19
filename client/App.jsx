2d3db60bcfebdcba90ea3ccf08b43d9ef682cf94
Ari Shoham <arishoham@aris-mbp.lan>
2022-02-19 17:19:37 -0500

    added is logged in middleware

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
+import GithubText from './GithubTest';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import Dashboard from './containers/Dashboard.jsx';
 
 function App() {
   return (
     <div className='App'>
+      <GithubText />
       <CssBaseline />
       {/* <h1>Test</h1> */}
       <Dashboard />
--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -5,51 +5,33 @@ var jwt = require('jsonwebtoken');
 const sessionController = {};
 
 //Check if user is already logged in
-// sessionController.isLoggedIn = (req, res, next) => {
-//   try {
-//     if(req.cookies.access_token) {
-//       var decoded = jwt.verify(req.cookies.access_token, process.env.SECRET_KEY);
-//       if(decoded.loggedIn === true) {
-//         res.locals.username = decoded.username;
-//         return next();
-//       }
-//       else {
-//         return res
-//           .clearCookie('access_token')
-//           .json('not logged in');
-//       }
-//     } else {
-//       return res
-//         .json('not logged in');
-//     }
-//   } catch(err) {
-//     return next({
-//       log: `Cannot check if user is logged in (sessionController) Err: ${err.message}`,
-//       status: 400,
-//       message: { err: 'An error occurred' },
-//     });
-//   }
-// };
-
-// sessionController.isLoggedInOptional = (req, res, next) => {
-//   try {
-//     if(req.cookies.access_token) {
-//       var decoded = jwt.verify(req.cookies.access_token, process.env.SECRET_KEY);
-//       if(decoded.loggedIn === true) {
-//         res.locals.username = decoded.username;
-//         return next();
-//       }
-//     }
-//     res.locals.username = false;
-//     return next();
-//   } catch(err) {
-//     return next({
-//       log: `Cannot check if user is logged in (optional) Err: ${err.message}`,
-//       status: 400,
-//       message: { err: 'An error occurred' },
-//     });
-//   }
-// };
+sessionController.isLoggedIn = (req, res, next) => {
+  try {
+    if(req.cookies.ssid) {
+      var decoded = jwt.verify(req.cookies.ssid, process.env.SECRET_KEY);
+      if(decoded.username !== undefined) {
+        res.locals.username = decoded.username;
+        return next();
+      }
+      else {
+        //JWT exists but is not verified
+        return res
+          .clearCookie('access_token')
+          .json('not logged in');
+      }
+      //JWT does not exist
+    } else {
+      return res
+        .json('not logged in');
+    }
+  } catch(err) {
+    return next({
+      log: `Cannot check if user is logged in (sessionController.isLoggedIn) Err: ${err.message}`,
+      status: 500,
+      message: { err: 'An error occurred' },
+    });
+  }
+};
 
 // add session JWT to cookies
 sessionController.startSession = (req, res, next) => {
@@ -62,7 +44,7 @@ sessionController.startSession = (req, res, next) => {
   } catch(err) {
     return next({
       log: `Cannot start session. Error in sessionController.startSession Err: ${err.message}`,
-      status: 400,
+      status: 500,
       message: { err: 'An error occurred' },
     });
   }
