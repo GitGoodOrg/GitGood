77f9d5e4c8c7547392b5f09333fc5ab9a4b27539
Ariel Lubin <ariel32lubin@gmail.com>
2022-02-23 14:02:20 -0600

    changes

--- client/index.js
+++ client/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 import "@fontsource/roboto";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
-render(<App />, document.getElementById("app"));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
